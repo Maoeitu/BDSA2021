69eda5fe0fe7845af1dbc6749cd36c494dfe3770
T4NKG0D <maoe@itu.dk>
2021-09-06 16:24:57 +0200

    Made the week 1 work properly

---
+++ Week1/Week1.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace Week1.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void print_Hello_World()
+        {
+        
+
+        }
+    }
+}
