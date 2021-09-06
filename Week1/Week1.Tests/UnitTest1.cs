2b6f5f63f5dfaef7546f6880f8e3660832efda73
T4NKG0D <maoe@itu.dk>
2021-09-06 17:33:01 +0200

    Implemeted Leap year method + tests

--- Week1/Week1.Tests/UnitTest1.cs
+++ Week1/Week1.Tests/UnitTest1.cs
@@ -1,15 +1,31 @@
 using System;
 using Xunit;
+using System.IO;
 
 namespace Week1.Tests
 {
     public class UnitTest1
     {
         [Fact]
-        public void print_Hello_World()
+        public void Is_Leap_Year()
         {
-        
-
+        var isLeapYear = Program.Is_Leap_Year(2020);
+        Assert.True(isLeapYear);
+        }
+        [Fact]
+        public void Is_Not_Leap_Year(){
+            var isNotLeapYear = Program.Is_Leap_Year(2021);
+            Assert.False(isNotLeapYear);
+        }
+        [Fact]
+        public void Exactly_Divisible_By_100(){
+            var IsNotALeapYear = Program.Is_Leap_Year(1700);
+            Assert.False(IsNotALeapYear);
+        }
+        [Fact]
+        public void Excactly_Divisible_By_400(){
+            var isLeapYear = Program.Is_Leap_Year(1600);
+            Assert.True(isLeapYear);
         }
     }
 }
--- Week1/Week1/Program.cs
+++ Week1/Week1/Program.cs
@@ -2,11 +2,31 @@
 
 namespace Week1
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+       public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Is_Leap_Year(2020);
+            
+            Console.WriteLine(Is_Leap_Year(2000));
+        }
+
+        public static bool Is_Leap_Year(int year){
+            if (year % 400 == 0) {
+                //If its leap year
+                return true;
+            }else if (year % 100 == 0){
+                //It is not leap year
+                return false;
+            }else if (year % 4 == 0){
+
+                //It is leap year
+                return true;
+            }
+            //If it is not leap year
+            return false;
+
+
         }
     }
 }
