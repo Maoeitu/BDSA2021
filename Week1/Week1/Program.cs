0bda3c221e8886770be1524b68b4ea67fb87b527
T4NKG0D <maoe@itu.dk>
2021-09-09 13:00:05 +0200

    Finished all the implementations from the assignment

--- Week1/Week1/Program.cs
+++ Week1/Week1/Program.cs
@@ -6,10 +6,32 @@ namespace Week1
     {
        public static void Main(string[] args)
         {
-            Is_Leap_Year(2020);
-            
-            Console.WriteLine(Is_Leap_Year(2000));
+
+            //Displays message to the user
+            Console.WriteLine("Enter a year!");
+
+            int converInput; 
+            var year = Console.ReadLine();
+            var succes = int.TryParse(year, out converInput);
+
+            if(succes == true){
+                if (converInput < 1582){
+                    Console.WriteLine("Please select a year higher then 1582");
+                
+                }else if (Is_Leap_Year(converInput)){
+                    Console.WriteLine("yay");
+                }else{
+                    Console.WriteLine("nay");
+                }
+            }else{
+                Console.WriteLine("Please enter year as a number!");
+            }
+        
         }
+           
+            
+            //Console.WriteLine(Is_Leap_Year(2000));
+        
 
         public static bool Is_Leap_Year(int year){
             if (year % 400 == 0) {
